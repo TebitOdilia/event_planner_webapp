b01ab9a17cb21890882637a8fa62e4806ce24d8c
TebitOdilia <115568823+TebitOdilia@users.noreply.github.com>
2023-05-23 05:37:14 +0100

    Add Event urls.py

--- Backend/event_planner/user_app/urls.py
+++ Backend/event_planner/user_app/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from knox.views import LogoutView, LogoutAllView
-from .views import login_view, register_view ,get_user_data
+from .views import login_view, register_view ,get_user_data, Event_view
 
 urlpatterns = [
     path('login/', login_view, name='login view'),
@@ -8,4 +8,6 @@ urlpatterns = [
     path('user/', get_user_data, name="user data"),
     path('logout/', LogoutView.as_view(), name="logout view"),
     path('logallout/', LogoutAllView.as_view(), name="logout all view"),
+    path('Event/', Event.as_view())
+
 ]
